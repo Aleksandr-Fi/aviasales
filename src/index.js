4e64cd208f6f2d973052d17a3aa5a04db87ef600
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2022-03-01 00:17:57 +0300

    add config redux-devtools
    and add redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
 
 import reducer from './reducer/reducer'
 import 'antd/dist/antd.css'
 import App from './components/App'
 
-const store = createStore(reducer)
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsDenylist, actionsCreators, serialize...
+      })
+    : compose
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunkMiddleware)))
 
 const update = () => {
   ReactDOM.render(
